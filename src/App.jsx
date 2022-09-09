aa1a8af4dd117141fdda642435e6fc3b8482ca3b
Dwi Waluyo Putranto <45709789+dwiiipoetra@users.noreply.github.com>
2022-09-09 16:58:25 +0700

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+// import components
+import Navbar from "../src/components/navbar";
+import Sidebar from "../src/components/sidebar";
+import Footer from "../src/components/footer";
+// import pages
+import Users from "./pages/users/index";
 import AddUsers from "./pages/users/addUsers";
 
 function App() {
-  return <AddUsers />;
+  return (
+    <div className="wrapper">
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path="/users" element={<Users />} />
+        <Route path="/add-users" element={<AddUsers />} />
+      </Routes>
+      <Users />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="main-footer">
+      <strong>
+        Copyright &copy; 2022
+        <a href="https://adminlte.io"> POS CAP </a>.
+      </strong>
+      All rights reserved.
+      <div className="float-right d-none d-sm-inline-block">
+        <b>Version</b> 1.0
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
